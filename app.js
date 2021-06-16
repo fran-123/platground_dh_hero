6602d138a1065b46df8f9ebcc5355b2127295750
franco-123 <francoromangimenez@gmail.com>
2021-06-16 06:51:54 -0300

    rutas de personas con relacion a informatica

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const app =express();
+const path =require("path");
+
+app.use(express.static("public"))
+
+app.listen(3030,()=>console.log( "servidor corriendo"));
+
+app.get("/home",(req,res)=>res.sendFile(path.join(__dirname,"views","index.html")));
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"views","index.html")))
+
+app.get("/babbage",(req,res)=>res.sendFile(path.join(__dirname,"views","babbage.html")))
+app.get("/clarke",(req,res)=>res.sendFile(path.join(__dirname,"views","clarke.html")));
+app.get("/hamilton",(req,res)=> res.sendFile(path.join(__dirname,"views","hamilton.html")));
+app.get("/hopper",(req,res)=>res.sendFile(path.join(__dirname,"views","hopper.html")));
+app.get("/lovelace",(req,res)=>res.sendFile(path.join(__dirname,"views","lovelace.html")));
+app.get("/turnig",(req,res)=>res.sendFile(path.join(__dirname,"views","turnig.html")));
